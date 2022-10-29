616639d684a5e2049db3dc01dd7b485bdeb302f3
MD.Omar Faruq <omarfaruq.bplus.6600@gmail.com>
2022-10-29 11:27:51 +0600

    second commet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+// add user
+const handleAddUser=(e)=>{
+  e.preventDefault()
+  
+  const name = e.target.name.value;
+  const email = e.target.email.value;
+  const phone = e.target.phone.value;
+  
+  //object create
+  const user = {name,email,phone}
+
+  // post data to server
+  fetch('http://localhost:5000/user', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+  .then(res => res.json())
+  .then(data => {
+    const newUsers = [...users, data];
+    setUsers(newUsers);
+    console.log(data)
+  });
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1>my own data</h1>
+      <h3>total data: {users.length}</h3>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" placeholder="name" name="name" required/>
+        <input type="text" placeholder="email" name="email" required/>
+        <input type="text" placeholder="phone" name="phone" required/>
+        <input type="submit" value="Add User"/>
+      </form>
+
+      <hr />
+      {users.map((user) => (
+        <p key={user.id}>
+          {user.name} & {user.email}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
     </div>
   );
 }
